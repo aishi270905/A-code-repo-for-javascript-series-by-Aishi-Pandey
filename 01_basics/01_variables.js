25fdcda0ec4c37ecb0f51ca1ccbdcab96ee2514a
aishi270905 <aishipandeyofficial@gmail.com>
2025-08-16 14:49:23 +0000

    Updated 01_variables.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 144553
+let accountEmail = "aishi@google.com"
+var accountpassword = "12345678"
+ accountCity = "Kolkata"
+// accountId = 2
+accountEmail="hc@hc.com"
+accountpassword = "87654321"
+console.log(accountId);
+console.table([accountId, accountEmail, accountpassword, accountCity]); 
