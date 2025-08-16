bed71eab91d3ab54d71a9f6d70b5f0b6aeff6a54
aishi270905 <aishipandeyofficial@gmail.com>
2025-08-16 18:18:46 +0000

    updated 03_conversionOperation.js

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,1 +1,14 @@
-
+"use strict"; //treat all js as newer version
+let name="Aishi"
+let age = 18
+let isLoggedin=false
+//number=>2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol =>unique
+console.log(typeof("Aishi"))
+console.log(typeof(age))
+console.log(typeof(null))
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = 33
+let score1 = "33"
+let score2 = "33abc"
+console.log(typeof(score))
+console.log(typeof(score1))
+let valueInNumber = Number(score1)
+let valueInNumber2 = Number(score2)
+console.log(typeof(valueInNumber))
+console.log(typeof(valueInNumber2))
+console.log(valueInNumber)
+console.log(valueInNumber2)
+//true => 1; false => 0
+let isLoggedin=1
+let booleanIsLoggedIn = Boolean(isLoggedin)
+console.log(booleanIsLoggedIn)
+//"" => false
+//"Aishi" => true
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(someNumber)
+console.log(typeof stringNumber)
