947369ab50f07f5df1967767247827b153aad8f9
aishi270905 <aishipandeyofficial@gmail.com>
2025-08-17 11:15:51 +0000

    datatypes-summary.js

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,25 @@
+//#Primitive
+//7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id == anotherId)
+const bigNumber = 2345678456789345678n //save as BigInt
+
+//Reference(Non-Primitive)
+//Array, Objects, Functions
+
+const heros = ["Shaktiman", "Nagraj", "Doga"] //Array
+let myObj = {
+    name: "Aishi";
+    age:19;
+} // object
+const myFunction = function (){
+    console.log("Hello world");
+}
+
+console.log(typeof bigNumber)
+
+
+//JAVASCRIPT IS DYNAMICALLY TYPED
+
