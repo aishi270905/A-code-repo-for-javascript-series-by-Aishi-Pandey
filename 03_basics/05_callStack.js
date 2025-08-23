1e87f2a24fecd0d762a1114f03bc5f9527d2fac2
aishi270905 <aishipandeyofficial@gmail.com>
2025-08-23 10:49:15 +0000

    about execution phase and call stck in 05_callStack.js

---
+++ 03_basics/05_callStack.js
@@ -0,0 +1,22 @@
+/*  execution context -----------> Javacript Execution Context
+                           |-----> Function Execution Context
+                           |-----> Eval Execution Context
+
+
+  ## Javacript Execution Context(when you put a file then how JS will run/execute the code)
+
+  (Javascript is a single threaded )
+
+    ** NOTES =>  when you give a code to the JS , one certain thing is always made that is: GLOBAL EXECUTION  CONTEXT. And whereever the Global EC is made, it will be refered to a variable called 'this'
+
+       JS code runs in two phases : 1) Memory Creation Phase (memory is allocated for the for the                                                    variables etc which are declared)
+                                    2) Execution Phase ( executions of everything takes place )
+    
+   [[[[the steps of execution phases : go to file execution_phase in this folder]]]]
+
+
+   
+   ##CALL STACK: 
+
+    **NOTES: if there is a set of functions called without executing previous ones, then it creates stack which during execution flows the LIFO rule.
+ */             
